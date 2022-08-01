625127e2810d5b2b0339da321b4fbdc890ecccc9
Addis Belete <addis.bel@gmail.com>
2022-08-01 21:23:45 +0300

    Add a receiptToken contract

---
+++ contracts/receiptToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20";
+
+contract receiptToken is ERC20 {
+    constructor() ERC20("Receipt token", "RT") {}
+
+    function mint(uint256 amount, address to) public {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount, address from) public {
+        _burn(from, amount);
+    }
+}

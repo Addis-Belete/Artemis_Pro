c10123d850d8e878fe3490c622c8cfdb2e7e1ceb
Addis Belete <addis.bel@gmail.com>
2022-08-01 21:13:44 +0300

    Add a constructor

--- contracts/pool.sol
+++ contracts/pool.sol
@@ -2,4 +2,12 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Pool {}
+contract Pool {
+    IERC20 public underyling; // any ERC20 predefined token
+    address public receiptToken; //receipt token
+
+    constructor(address _predefinedTokenAddress, address receiptTokensAddress) {
+        underyling = IERC20(_predefinedTokenAddress);
+        receiptToken = receiptTokensAddress;
+    }
+}

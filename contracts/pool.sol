87a7f3af0686f90fd6f5524864ac75ca0c6710d0
Addis Belete <addis.bel@gmail.com>
2022-08-01 21:21:54 +0300

    Update deposit function

--- contracts/pool.sol
+++ contracts/pool.sol
@@ -23,5 +23,21 @@ contract Pool {
 @param amount - amount of predefined token
 */
 
-    function deposit(uint256 amount) public {}
+    function deposit(uint256 amount) public {
+        require(
+            underyling.balanceOf(msg.sender) >= amount,
+            "You don't have enough amount to withdraw"
+        );
+        underyling.transfer(address(this), amount);
+        if (userData[msg.sender].isDeposited == true) {
+            uint256 initialAmount = userData[msg.sender].amount;
+            uint256 initialTime = userData[msg.sender].dep_started;
+            uint256 reward = calculateReward(initialAmount, initialTime);
+            uint256 finalAmount = initialAmount + amount + reward;
+            userData[msg.sender] = userInfo(finalAmount, block.timestamp, true);
+        } else {
+            userData[msg.sender] = userInfo(amount, block.timestamp, true);
+        }
+        IreceiptToken(receiptTokenAddress).mint(amount, msg.sender);
+    }
 }

a3f141ee548754654a1e55837469df0c76fd70f4
Addis Belete <addis.bel@gmail.com>
2022-08-03 10:09:51 +0300

    update hardhat.config.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,7 +17,7 @@ module.exports = {
 	},
 	networks: {
 		goerli: {
-			url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+			url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
 			accounts: [`0x${GOERLI_PRIVATE_KEY}`]
 		}
 	}
